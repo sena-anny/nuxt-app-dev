c711819d73d22a1d301f2af7959f1a4e5be7f9db
KJ <cozykoji18@gmail.com>
2019-10-13 05:40:41 +0900

    nuxt.js setup

---
+++ pages/users/_id.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="container">
+        <div>
+            <h3>{{user.id}}</h3>
+            <img :src="user.profile_image_url" width="120" alt="">
+            <p>{{user.description || 'No description'}}</p>
+            <p>
+                <nuxt-link to="/">
+                    <small><b>トップへ戻る</b></small>
+                </nuxt-link>
+            </p>
+            <h3>{{user.id}}さんの投稿一覧</h3>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  async asyncData({route, app}){
+    const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
+    const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
+    return {
+      user,items
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+    min-height: 100vh;
+    padding: 16px;
+}
+h3 {
+    margin: 16px 0;
+    padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 8px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+</style>
