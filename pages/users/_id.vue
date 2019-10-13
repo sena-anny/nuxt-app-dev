d129dde7875ea8ac9c1541d1e84de76e034dd59f
KJ <cozykoji18@gmail.com>
2019-10-13 23:23:04 +0900

    add head and vuex store

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -15,13 +15,34 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  async asyncData({route, app}){
-    const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
-    const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
-    return {
-      user,items
-    }
+    head() {
+        return {
+            title: this.user.id,
+            //meta: [{charset: "euc_jp"}],
+        }
+    },
+  async asyncData({route, store, redirect}){
+      if (store.getters['users'][route.params.id]) {
+          return
+      }
+      try {
+          await store.dispatch('fetchUserInfo', {id: route.params.id})
+      }catch (e) {
+          console.log("error")
+          redirect('/')
+      }
+  },
+  computed: {
+      user() {
+          return this.users[this.$route.params.id]
+      },
+      items() {
+          return this.userItems[this.$route.params.id] || []
+      },
+      ...mapGetters(['users','userItems'])
   }
 }
 </script>
